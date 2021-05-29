797eeb284c85910a59bf7be8c50d027029f41e06
Ayush Vijayant <ayush.vijayant@gmail.com>
2021-05-29 14:17:29 +0530

    Handle post request

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,6 @@
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
   end
@@ -15,6 +17,5 @@ class TodosController < ApplicationController
     new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
     response_text = "Hey your new todo is created with id #{new_todo.id}"
     render plain: response_text
-    # render plain: "Hey the params are #{params.to_s}"
   end
 end
