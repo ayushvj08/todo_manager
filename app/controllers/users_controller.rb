6f0167f7ad89b8941960543d17028c4551f34b5f
Ayush Vijayant <ayush.vijayant@gmail.com>
2021-05-30 19:51:42 +0530

    Create a new user by sending a POST request to /users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,13 @@ class UsersController < ApplicationController
   def index
     render plain: User.all.map { |user| user.to_pleasant_string }.join("\n")
   end
+
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    new_user = User.create!(name: name, email: email, password: password)
+    response_text = "A new user has been created with id #{new_user.id}"
+    render plain: response_text
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   resources :todos
   get "/users", to: "users#index"
+  post "/users", to: "users#create"
 end
